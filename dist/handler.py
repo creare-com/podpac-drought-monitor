fd08386de286bff6455f21a53a98b2f6a640bc83
Matt Ueckermann <mpu@creare.com>
2019-12-02 12:37:58 -0500

    ENH: Updating the smap-updater handler function and creating a lambda function for it as well.

--- dist/handler.py
+++ dist/handler.py
@@ -4,6 +4,12 @@ PODPAC-DROUGHT-MONITOR SMAP-DATA-UPDATER AWS Handler
 
 import sys
 import os
+import subprocess
+
+import boto3
+import botocore
+
+from six import string_types
 
 def handler(event, context):
     """Lambda function handler
@@ -18,22 +24,23 @@ def handler(event, context):
     print(event)
     
     sys.path.append("/tmp/")
-    bucket = 'podpac-internal-test'
+    bucket = 'podpac-drought-monitor-s3'
 
     # get dependencies path
     if "FUNCTION_DEPENDENCIES_KEY" in os.environ:
         dependencies = os.environ["FUNCTION_DEPENDENCIES_KEY"]
     else:
         dependencies = "podpac_deps_{}.zip".format(
-            'os.environ["PODPAC_VERSION"]'
+            os.environ.get("PODPAC_VERSION", '1.2.0')
         )  # this should be equivalent to version.semver()
 
     # Download dependencies from specific bucket/object
-    s3 = boto3.client("s3")
-    s3.download_file(bucket, dependencies, "/tmp/" + dependencies)
-    subprocess.call(["unzip", "/tmp/" + dependencies, "-d", "/tmp"])
-    sys.path.append("/tmp/")
-    subprocess.call(["rm", "/tmp/" + dependencies])
+    if event:
+        s3 = boto3.client("s3")
+        s3.download_file(bucket, dependencies, "/tmp/" + dependencies)
+        subprocess.call(["unzip", "/tmp/" + dependencies, "-d", "/tmp"])
+        sys.path.append("/tmp/")
+        subprocess.call(["rm", "/tmp/" + dependencies])
     
     
     import matplotlib
@@ -43,6 +50,7 @@ def handler(event, context):
     import podpac
     import xarray as xr
     import zarr
+    import numpy as np
 
     from podpac import settings
     import podpac.datalib
@@ -70,27 +78,39 @@ def handler(event, context):
     # L4 data
     print("Downloading the new L4 Data")
     smap = smap_egi.SMAP(product='SPL4SMAU')
-    l4 = smap.eval(coords_l4)
-    l4.set(-9999, np.isnan(l4))
     time_base = xr.coding.times.decode_cf_datetime(0, smap_ds_L4['time'].attrs['units'])
-    new_times = (coords_l4.coords['time'].data - time_base).astype('timedelta64[D]')
-
-    I = new_times > smap_ds_L4['time'][:].max()
-    old_time_shape = smap_ds_L4['time'].shape
-    old_data_shape = smap_ds_L4['Geophysical_Data_sm_surface'].shape
+    count = 0 
+    for c in coords_l4.iterchunks((1, ) + coords_l4.shape[1:]): 
+        count += 1
+        if count == 1: 
+            continue # start is already in dataset
+        print("Download L4 Data for:", str(c['time']), '...', end='')
+        try: 
+            l4 = smap.eval(c)
+        except ValueError as e:
+            print("No Granules available:", e)
+            continue
+        l4.set(-9999, podpac.UnitsDataArray(np.isnan(l4)))
+        print(" ... Done.")
+        new_times = (c.coords['time'].data - time_base).astype('timedelta64[D]')
+        for i, nt in enumerate(new_times):
+            if np.any((smap_ds_L4['time'][:] - nt).astype(int) >= 0) or np.all(np.isnan(l4[i])):
+                print('Time already exists, or is all nan -- skipping.')
+                continue
+            print("Updating S3 Zarr file for L4 Data.")
+            old_time_shape = smap_ds_L4['time'].shape
+            old_data_shape = smap_ds_L4['Geophysical_Data_sm_surface'].shape
+            print ('Old Shape:', old_data_shape)
+            try: 
+                smap_ds_L4['time'].append(np.atleast_1d(nt))
+                smap_ds_L4['Geophysical_Data_sm_surface'].append(l4[i:i+1], axis=0)
+            except Exception as e:
+                print ('Updating L4 data failed:', e)
+                smap_ds_L4['time'].resize(*old_time_shape)
+                smap_ds_L4['Geophysical_Data_sm_surface'].resize(*old_data_shape)
+            print ('New Shape:', smap_ds_L4['Geophysical_Data_sm_surface'].shape)
+        # if count >= 0: break  # uncomment to test L3 below
     
-    print ('Updating {} new times for L4 data.'.format(I.sum()))
-    try: 
-        # Update the zarr file
-        ## IMPORTANT ONLY EXECUTE THIS CELL ONCE!!!! ANY ERRORS? DO NOT EXECUTE THE SAME APPEND AGAIN!
-        smap_ds_L4['time'].append(new_times[I])
-        smap_ds_L4['Geophysical_Data_sm_surface'].append(l4[I], axis=0)
-    except as e:  # Fails silently
-        print ('Updating L4 data failed:', e)
-        smap_ds_L4['time'].reshape(old_time_shape)
-        smap_ds_L4['Geophysical_Data_sm_surface'].reshape(old_data_shape)
-
-
     # AM/PM Data
     print("Opening L3 Zarr dataset.")
     smap_zarr = podpac.data.Zarr(source=source, datakey='Soil_Moisture_Retrieval_Data_AM_soil_moisture', file_mode='a', **kwargs)
@@ -99,50 +119,70 @@ def handler(event, context):
     print("Creating L3 Coordinates")
     coords = smap_zarr.native_coordinates.drop('time')
     time = podpac.crange(smap_zarr.native_coordinates['time'].bounds[-1], str(datetime.date.today()), '1,D', 'time')
-    coords = podpac.coordinates.merge_dims([podpac.Coordinates([time], crs=coor
+    coords = podpac.coordinates.merge_dims([podpac.Coordinates([time], crs=coords.crs), coords])
 
-    # AM Data
-    print("Downloading the new L4 Data")
-    smap = smap_egi.SMAP(product='SPL3SMP_E_AM', check_quality_flags=False)
-    l3_am = smap.eval(coords)
-    smap.set_trait('data_key', smap.quality_flag_key)
-    l3_am_qf = smap.eval(coords)
-
-    # PM Data
-    smap = smap_egi.SMAP(product='SPL3SMP_E_PM', check_quality_flags=False)
-    l3_pm = smap.eval(coords)
-    smap.set_trait('data_key', smap.quality_flag_key)
-    l3_pm_qf = smap.eval(coords)
-
-    # replace_nans
-    l3_am.set(-9999, np.isnan(l3_am))
-    l3_am_qf.set(-9999, np.isnan(l3_am_qf))
-    l3_pm.set(-9999, np.isnan(l3_pm))
-    l3_pm_qf.set(-9999, np.isnan(l3_pm_qf))
-
+    print("Downloading the new L3 Data")
     time_base = xr.coding.times.decode_cf_datetime(0, smap_ds['time'].attrs['units'])
-    new_times = (coords.coords['time'].data - time_base).astype('timedelta64[D]')
-    I = new_times > smap_ds['time'][:].max()
-    old_time_shape = smap_ds['time'].shape
-    old_data_shape = smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].shape
-                                                               
-    print ('Updating {} new times for L4 data.'.format(I.sum()))
+    count = 0
+    for c in coords.iterchunks((1, ) + coords.shape[1:]): 
+        count += 1                                                 
+        if count == 1: continue  # Start is already in the dataset
+        print("Download L3 Data for:", str(c['time']), '...', end='')
+        try:
+            # AM Data
+            print("AM", end=' ')
+            smap = smap_egi.SMAP(product='SPL3SMP_E_AM', check_quality_flags=False)
+            l3_am = smap.eval(c)
+            smap.set_trait('data_key', smap.quality_flag_key)
+            print("AM_QF", end=' ')
+            l3_am_qf = smap.eval(c)
 
-    try:
-        # Update the zarr file
-        ## IMPORTANT ONLY EXECUTE THIS CELL ONCE!!!! ANY ERRORS? DO NOT EXECUTE THE SAME APPEND AGAIN!
-        smap_ds['time'].append(new_times)
-        smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].append(l3_am[1:], axis=0)
-        smap_ds['Soil_Moisture_Retrieval_Data_AM_retrieval_qual_flag'].append(l3_am_qf[1:], axis=0)
-        smap_ds['Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm'].append(l3_pm[1:], axis=0)
-        smap_ds['Soil_Moisture_Retrieval_Data_PM_retrieval_qual_flag_pm'].append(l3_pm_qf[1:], axis=0)
-    except as e:
-        print ('Updating L3 data failed:', e)
-        smap_ds['time'].reshape(old_time_shape)
-        smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].reshape(old_data_shape)
-        smap_ds['Soil_Moisture_Retrieval_Data_AM_retrieval_qual_flag'].reshape(old_data_shape)
-        smap_ds['Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm'].reshape(old_data_shape)
-        smap_ds['Soil_Moisture_Retrieval_Data_PM_retrieval_qual_flag_pm'].reshape(old_data_shape)
-                                                               
+            # PM Data
+            print("PM", end=' ')
+            smap = smap_egi.SMAP(product='SPL3SMP_E_PM', check_quality_flags=False)
+            l3_pm = smap.eval(c)
+            smap.set_trait('data_key', smap.quality_flag_key)
+            print("PM_QF", end=' ')
+            l3_pm_qf = smap.eval(c)
+        except ValueError as e:
+            print("No Granules available:", e)
+            continue
+
+        # replace_nans
+        l3_am.set(-9999, podpac.UnitsDataArray(np.isnan(l3_am)))
+        l3_am_qf.set(-9999, podpac.UnitsDataArray(np.isnan(l3_am_qf)))
+        l3_pm.set(-9999, podpac.UnitsDataArray(np.isnan(l3_pm)))
+        l3_pm_qf.set(-9999, podpac.UnitsDataArray(np.isnan(l3_pm_qf)))
+
+        print(" ... Done.")
+        new_times = (c.coords['time'].data - time_base).astype('timedelta64[D]')
+        for i, nt in enumerate(new_times):
+            if np.any((smap_ds['time'][:] - nt).astype(int) >= 0) or np.all(np.isnan(l3_am)) or np.all(np.isnan(l3_pm)):
+                print('Time already exists, or is all nan -- skipping.')
+                continue
+
+            print("Updating S3 Zarr file for L3 Data.")
+            old_time_shape = smap_ds['time'].shape
+            old_data_shape = smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].shape
+
+            print ('Old Shape:', old_data_shape)
+            try:
+                # Update the zarr file
+                ## IMPORTANT ONLY EXECUTE THIS CELL ONCE!!!! ANY ERRORS? DO NOT EXECUTE THE SAME APPEND AGAIN!
+                smap_ds['time'].append(np.atleast_1d(nt))
+                smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].append(l3_am[i:i+1], axis=0)
+                smap_ds['Soil_Moisture_Retrieval_Data_AM_retrieval_qual_flag'].append(l3_am_qf[i:i+1], axis=0)
+                smap_ds['Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm'].append(l3_pm[i:i+1], axis=0)
+                smap_ds['Soil_Moisture_Retrieval_Data_PM_retrieval_qual_flag_pm'].append(l3_pm_qf[i:i+1], axis=0)
+            except Exception as e:
+                print ('Updating L3 data failed:', e)
+                smap_ds['time'].reshape(*old_time_shape)
+                smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].reshape(*old_data_shape)
+                smap_ds['Soil_Moisture_Retrieval_Data_AM_retrieval_qual_flag'].reshape(*old_data_shape)
+                smap_ds['Soil_Moisture_Retrieval_Data_PM_soil_moisture_pm'].reshape(*old_data_shape)
+                smap_ds['Soil_Moisture_Retrieval_Data_PM_retrieval_qual_flag_pm'].reshape(*old_data_shape)
+            print ('New Shape:', smap_ds['Soil_Moisture_Retrieval_Data_AM_soil_moisture'].shape)
     return
 
+if __name__ == '__main__':
+    handler(None, None)
